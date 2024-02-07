558067d675784585fb0e595e1bbb31e065f46dc8
Andrea Stagi <stagi.andrea@gmail.com>
2024-02-07 18:46:48 +0100

    feat: first header import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { Alert } from 'design-react-kit';
+import CompleteHeader from "./CompleteHeader"
 import './App.css';
+import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CompleteHeader></CompleteHeader>
     </div>
   );
 }
